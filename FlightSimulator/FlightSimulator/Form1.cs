289300b8a0c256a420ff374bfe05f1e4a8b67cbd
mickeyngobeni <mickeyngobeni@gmail.com>
2019-04-03 19:36:25 +0200

    Entered a prototype for the map - Mika. Commit

--- FlightSimulator/FlightSimulator/Form1.cs
+++ FlightSimulator/FlightSimulator/Form1.cs
@@ -16,5 +16,22 @@ namespace FlightSimulator
         {
             InitializeComponent();
         }
+
+        int x, y = 0; 
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            x = e.X; //Assigning variable x to the X coordinate of the mouse whenever it moves
+            y = e.Y; //Same with x
+
+            label1.Location = new Point(x - label1.Width, y); //Not entirely sure why width is minused
+                                                              //Essentially, it's moving the label to where the mouse cursor is
+            label2.Location = new Point(x, y - label2.Height);
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+        }
+
     }
 }
